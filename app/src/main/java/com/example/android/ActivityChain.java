60e0e6baf95b1c8ea24e39166aa33a73fa45d1ff
ndienstknecht <dienstknecht@se-rwth.de>
2019-05-03 12:41:09 +0200

    first design

---
+++ app/src/main/java/com/example/android/ActivityChain.java
@@ -0,0 +1,23 @@
+package com.example.android;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ActivityChain extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        return null;
+    }
+
+    public void onClickPlusButton(View v){
+
+    }
+
+    public void onClickSaveButton(View v){
+
+    }
+
+}
--- app/src/main/java/com/example/android/MainActivity.java
+++ app/src/main/java/com/example/android/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.android;
 
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,47 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if(savedInstanceState==null){
+            getSupportFragmentManager().beginTransaction().add(R.id.container, new ActivityChainFragment()).commit();
+        }
+        Button btStart = findViewById(R.id.btStart);
+        btStart.setOnClickListener(this::onClickStart);
+        Button btPlus = findViewById(R.id.btPlus);
+        btStart.setOnClickListener(this::onClickPlus);
+        Button btSave = findViewById(R.id.btSave);
+        btStart.setOnClickListener(this::onClickSave);
+    }
+
+    public void onClickStart(View v){
+
+    }
+
+    public void onClickPlus(View v){
+
+    }
+
+    public void onClickSave(View v){
+
+    }
+
+    public static class ActivityChainFragment extends Fragment {
+        public ActivityChainFragment(){
+
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+            View rootView = inflater.inflate(R.layout.activity_activitychain,container,false);
+            return rootView;
+        }
+
+        public void onClickPlusButton(View v){
+
+        }
+
+        public void onClickSaveButton(View v){
+
+        }
     }
 }
