7ce7c7adefa0c0b83f25c745f5bd561d423766a8
ndienstknecht <dienstknecht@se-rwth.de>
2019-05-10 10:07:26 +0200

    toolbar

--- app/src/main/java/com/example/android/MainActivity.java
+++ app/src/main/java/com/example/android/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.android;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -24,6 +29,9 @@ public class MainActivity extends AppCompatActivity {
         btStart.setOnClickListener(this::onClickPlus);
         Button btSave = findViewById(R.id.btSave);
         btStart.setOnClickListener(this::onClickSave);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 
     public void onClickStart(View v){
@@ -56,5 +64,12 @@ public class MainActivity extends AppCompatActivity {
         public void onClickSaveButton(View v){
 
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu, this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
     }
 }
